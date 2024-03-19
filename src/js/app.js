dd9102ff25051e25d470aa43fe222116c45c5300
leobellorin <jlbellorin24@gmail.com>
2024-03-19 09:29:09 +0100

    da

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+
+
+
+
+const formulario = document.querySelector("#formulario");
+const selectmonedas = document.querySelector("selectmonedas");
+const selectcryptos = document.querySelector("select#criptomonedas");
+const divresultado = document.querySelector("#resultado");
+
+
+document.addEventListener(`DOMContentloaded`, iniciarAPP);
+
+
+async function iniciarAPP(){
+
+    await obtenerCriptomonedas();
+}
+async function obtenerCriptomonedas(){
+    console.log("obteniendo crypto monedas...")
+}
+
+async function obtenerCriptomonedas(){
+
+    let resultado = null;
+
+    try {
+        const respuesta = await fetch(url);
+        const respuesta = await respuesta.json();
+    } catch (error){
+
+    } finally {
+        return resultado;
+    }
+}
