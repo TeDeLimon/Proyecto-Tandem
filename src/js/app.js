d113b796783e19a64af7551a210549c6c0081d82
Luis Villanueva <106811528+TeDeLimon@users.noreply.github.com>
2024-03-19 09:28:49 +0100

    JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 //Variables del documento JS
-const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+let criptomonedas;
 
 //Selectores del documento
 const formulario = document.querySelector("#formulario");
@@ -10,11 +12,12 @@ const divResultado = document.querySelector("#resultado");
 //Event listeners
 document.addEventListener('DOMContentLoaded', iniciarAPP);
 
-
 async function iniciarAPP() {
 
     //Consultar las criptomonedas disponibles
-    await obtenerCriptomonedas();
+    criptomonedas = await obtenerCriptomonedas();
+
+    console.log(criptomonedas);
 
     //Incorporarlas al select de criptomonedas
 
@@ -22,5 +25,20 @@ async function iniciarAPP() {
 }
 
 async function obtenerCriptomonedas() {
-    
+
+    let resultado = null;
+
+    try {
+
+        const respuesta = await fetch(url);
+        resultado = await respuesta.json();
+
+    } catch (error) {
+
+        console.log(error);
+
+    } finally {
+
+        return resultado;
+    }
 }
